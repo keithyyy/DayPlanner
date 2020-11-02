1d6923a89648513c1c77f641e575bfd42d80000a
Keith Crooc <keith.crooc@gmail.com>
2020-11-02 13:34:38 -0500

    created loop that adds the slot

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ $(document).ready(function() {
     $('#currentDay').text(dateDisplay);
 
 
+    var timeslots = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM'];
 
+    for (let i = 0; i < timeslots.length; i++) {
+        var slot = $("<div>");
+        slot.addClass("row time-block hour description textarea");
+        slot.attr('data-number', timeslots[i]);
+        slot.text(timeslots[i]);
+        $('.container').append(slot);
+    }
 
 
 
